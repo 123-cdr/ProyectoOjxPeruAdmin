d8b02a678c0ef6e012651d23fefc18926d7a1e54
Davis.cdr <thony.cdr@hotmail.com>
2020-11-04 09:38:55 -0500

    cambio inicio

---
+++ src/Conexion/ConexionSQL.java
@@ -0,0 +1,36 @@
+
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/*
+ * @author Davis
+ */
+
+public class ConexionSQL {
+    
+    public static Connection conexionn() {
+        Connection conect = null;
+        try {
+            String controlador = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+            String driver = "jdbc:sqlserver://localhost:1433;databaseName=OjxPeru";
+            String user = "sa";
+            String pass = "123";
+            try {
+                Class.forName(controlador);
+                conect = DriverManager.getConnection(driver, user, pass);
+                if(conect!=null){
+                    System.out.println("Conexion Ok");
+                }
+            } catch (Exception e) {
+            System.out.println("Error Conexion: "+e.getMessage());
+            }  
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error "+e);
+        }
+        return conect;
+    }
+    
+}
